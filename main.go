75c255471a5f3bbbc642f8539686b3111f8b6027
Ban Hun <banhun@Banui-iMac.local>
2023-03-09 18:39:49 +0900

    webhook secret test2

--- main.go
+++ main.go
@@ -89,7 +89,7 @@ func rootHandler(db *sql.DB, c echo.Context) error {
 	if err != nil {
 		return c.HTML(http.StatusInternalServerError, err.Error())
 	}
-	return c.HTML(http.StatusOK, fmt.Sprintf("Hello, Docker! -v10-replicas test3 (%d)\n", r))
+	return c.HTML(http.StatusOK, fmt.Sprintf("Hello, Docker! -v10-webhook secret test (%d)\n", r))
 }
 
 func sendHandler(db *sql.DB, c echo.Context) error {
